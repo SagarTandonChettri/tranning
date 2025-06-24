a04083dc3c3eb4a2f5340d1454612760590b18fa
SagarTandonChettri <145041435+SagarTandonChettri@users.noreply.github.com>
2025-06-24 15:31:22 +0530

    Delete tournamentApplication directory

--- tournamentApplication/src/main/java/com/tournamentApplication/tournamentApplication/service/StudentApiService.java
+++
@@ -1,9 +0,0 @@
-package com.tournamentApplication.tournamentApplication.service;
-
-import com.tournamentApplication.tournamentApplication.dto.StudentApiDto;
-
-import java.util.List;
-
-public interface StudentApiService {
-    List<StudentApiDto> getAllStudent();
-}
